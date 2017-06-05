62d7b7e8612eaf145960287e7ced2ac237998925
Forrest VanderBorgh <forrest.vanderborgh@flightly.com>
2017-06-04 23:14:15 -0700

    Basic Token Generation

--- app/components/subscribe.js
+++ app/components/subscribe.js
@@ -29,24 +29,40 @@ const styles = StyleSheet.create({
   }
 });
 
-class Subscribe extends Component{
-  constructor(props){
+class Subscribe extends Component {
+  constructor(props) {
     super(props);
+    this.state = {
+      stripe: {
+        url: 'https://api.stripe.com/v1/tokens?',
+        secretKey: 'sk_test_6H3ofPJjlfkybRNYLchsyYYj'
+      },
+      cardDetails: {
+        "card[number]": '4242424242424242',
+        "card[exp_month]": '01',
+        "card[exp_year]": '2020',
+        "card[cvc]": '123'
+      }
+    };
     this._onPressOneTime = this._onPressOneTime.bind(this);
     this._renderOneTimeButton = this._renderOneTimeButton.bind(this);
 
     this._onPressSubscription = this._onPressSubscription.bind(this);
     this._renderSubscriptionButton = this._renderSubscriptionButton.bind(this);
+
+    this._handleCreateCardToken = this._handleCreateCardToken.bind(this);
+
   }
 
   _onPressOneTime() {
 
+    this._handleCreateCardToken();
   }
 
   _renderOneTimeButton() {
     const oneTimePrompt = 'Send one fact';
     return (
-      <TouchableOpacity 
+      <TouchableOpacity
         onPress={this._onPressOneTime}
       >
         <Text>
@@ -63,7 +79,7 @@ class Subscribe extends Component{
   _renderSubscriptionButton() {
     const subscriptionPrompt = 'Subscribe for multiple facts';
     return (
-      <TouchableOpacity 
+      <TouchableOpacity
         onPress={this._onPressOneTime}
       >
         <Text>
@@ -73,9 +89,37 @@ class Subscribe extends Component{
     )
   }
 
+  _handleCreateCardToken() {
+    let formBody = [];
+    for (let property in this.state.cardDetails) {
+      let encodedKey = encodeURIComponent(property);
+      let encodedValue = encodeURIComponent(this.state.cardDetails[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    debugger;
+
+    return (
+      fetch(this.state.stripe.url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Bearer ' + this.state.stripe.secretKey
+        },
+        body: formBody
+      }).then(
+        function(res) {
+        }
+      )
+    );
+  }
+
   render() {
     const renderOneTime = this._renderOneTimeButton();
     const renderSubscription = this._renderSubscriptionButton();
+
     return (
       <View style={styles.container}>
         {renderOneTime}
